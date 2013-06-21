5b853f6b658fe6df7a5c9165df28f2a308346390
Kuldeep Sinh <chauhan.kuldeep.sinh@gmail.com>
2013-06-21 15:16:44 +0200

    Addded some basic tests related to simple_types and objects.

---
+++ 2_objects/spec/are_typeof_Objects.spec.js
@@ -0,0 +1,23 @@
+describe("typeof Objects: ", function()
+{
+    it("objects are typeof objects", function()
+       {
+	   var o = {};
+	   expect(typeof(o)).toEqual("object");
+       });
+    it("arrays are typeof objects", function()
+       {
+	   var a = [1,2,3];
+	   expect(typeof(a)).toEqual("object");
+       });
+    it("functions are typeof function", function()
+       {
+	   var f = function() {};	   
+	   expect(typeof(f)).toEqual("function");
+       });
+    it("regular expressions are typeof function", function()
+       {
+	   var r = /test/;	   
+	   expect(typeof(r)).toEqual("object");
+       });
+});
---
+++ 2_objects/spec/obj_retrieval.spec.js
@@ -0,0 +1,35 @@
+describe("Retrieve values from object : ", function()
+{
+    it('Value of an object property can be retrieve using following syntax : obj["key"].', function()
+       {
+	   var o = {key:"value"};
+	   expect(o["key"]).toEqual("value");
+       });
+    it('Value of an object property can be retrieve using following syntax : obj.key.', function()
+       {
+	   var o = {key:"value"};
+	   expect(o.key).toEqual("value");
+       });
+    it('If the property/key does not exist, value = undefined is return.', function()
+       {
+	   var o = {key:"value"};
+	   expect(o.foo).toEqual(undefined);
+       });
+    it('If the property/key does not exist, || operator can be used to set default value.', function()
+       {
+	   var o = {key:"value"};
+	   var f = o.foo || "Not Available";
+	   expect(f).toEqual("Not Available");
+       });
+    it('Attempting to retrieve values from an undefined property will throw a TypeError exception.', function()
+       {
+	   var o = {key:"value"};
+	   var f = function() 
+	   {
+	       try { return o.f.bar; }
+	       catch(err) { throw err; }
+	   };
+	   expect(f).toThrow();
+       });
+});
+
