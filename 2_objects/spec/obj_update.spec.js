557b204217b31011c85184787b4d7f889b64af5f
Kuldeep Sinh Chauhan <chauhan.kuldeep.sinh@gmail.com>
2013-06-21 15:42:26 +0200

    Update obj_update.spec.js

--- 2_objects/spec/obj_update.spec.js
+++ 2_objects/spec/obj_update.spec.js
@@ -5,6 +5,7 @@ describe("Update Object : ", function()
 	   var o = {key : "value"};
 	   o.key = "new_value";
 	   expect(o.key).toEqual("new_value");
+	   expect(o.key).not.toEqual("value");
        });
     it("Assigning new value to a non-existing property, accumulates new property to the object.", function()
        {
