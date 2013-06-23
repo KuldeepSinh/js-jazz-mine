1d53dfd157ef5c9dd26851de3d66bd3c48902bc7
Kuldeep Sinh <chauhan.kuldeep.sinh@gmail.com>
2013-06-23 14:20:29 +0200

    Scope related tests are moved to closure, as they are related to it.

--- 3_functions/spec/closure.spec.js
+++ 3_functions/spec/closure.spec.js
@@ -2,7 +2,7 @@ describe("Closure : ", function()
 {
     //variable defined at the function named describe.
     var a = 1;
-    it("Inner fuction has access to the variables defined in the outer function.", function()
+    it("Inner function has access to the variables defined in the outer function.", function()
        {
 	   //variable defined at the function named it.
 	   var b = 2;
@@ -19,5 +19,66 @@ describe("Closure : ", function()
 	       return inner(); 
 	   }
 	   expect(outer()).toEqual(10);	   	   
+       });
+    it("Inner function overrides variables defined in the outer function", function()
+       {
+	   function outer()
+	   {
+	       var a = 1, b = 2;
+	       function inner()
+	       {
+		   var b = 3, c = 4;
+		   return a + b + c;
+	       }
+	       return inner(); 
+	   }
+	   expect(outer()).toEqual(8);	   
+       });
+    it("Outer function does not have access to variables defined in the inner function", function()
+       {
+	   function outer()
+	   {
+	       var a = 1, b = 2;
+	       function inner()
+	       {
+		   var b = 3, c = 4;
+		   return a + b + c;
+	       }
+	       return a + b; 
+	   }
+	   expect(outer()).toEqual(3);	   
+       });
+    it("If outer function tries to access any variable defined in the inner function, exceptoin is thrown.", function()
+       {
+	   function outer()
+	   {
+	       var a = 1;
+	       function inner()
+	       {
+		   var b;
+	       }
+	       return b; 
+	   }
+	   expect(outer).toThrow();	   
+       });
+
+    it("Inner function can be referenced before and after its definition.", function()
+       {
+	   function outer()
+	   {
+	       //inner is called before it is defined.
+	       expect(typeof(inner)).toBe("function");
+	       function inner() {}
+	       expect(typeof(inner)).toBe("function");
+	   }
+	   outer();
+       });
+    it("Inner function can not be referenced outside the outer function.", function()
+       {
+	   function outer()
+	   {        
+	       function inner() {}
+	   }
+	   expect(typeof(inner)).toBe("undefined");
        });
 });
