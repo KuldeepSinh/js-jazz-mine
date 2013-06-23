754636936e24c6903d6e26d7af451af7f2618aa6
Kuldeep Sinh <chauhan.kuldeep.sinh@gmail.com>
2013-06-23 16:42:36 +0200

    tests addef for invocation

--- 3_functions/spec/arguments.spec.js
+++ 3_functions/spec/arguments.spec.js
@@ -1,4 +1,4 @@
-describe("arguments object : ", function()
+describe("Function arguments : ", function()
 {
     //sum is a function, which takes advantage of the arguments object,
     //to sum all the arguments passed to it.
@@ -19,5 +19,15 @@ describe("arguments object : ", function()
     it("arguments object contains all the arguments, when function is called with argument.", function()
        {
 	   expect(sum(1,2,3,4,5)).toEqual(15);
+       });
+    it("If more arguments are passed than required, execive arguments are ignored.", function()
+       {
+	   function add(a, b) { return a+b; }
+	   expect(add(1,2,3,4,5)).toEqual(3);
+       });
+    it("If less arguments are passed than required, remaining arguments remain undefined.", function()
+       {
+	   function arg_type(a) { return typeof(a); }
+	   expect(arg_type()).toEqual("undefined");
        });
 });
