fa34af904c931c5a5b94636756e51e919a06ee4d
Kuldeep Sinh <chauhan.kuldeep.sinh@gmail.com>
2013-06-24 13:39:11 +0200

    added tests for various kinds of function invocations.

--- 3_functions/spec/function_invocation.spec.js
+++ 3_functions/spec/function_invocation.spec.js
@@ -4,12 +4,18 @@ message = "Hello, World!!!";
 //When a function is not a property of an object, it is invoked as a function.
 describe("Function Invocation : ", function()
 {
-    it("When a function is not a property, it can be invoked directly with name.", function()
+    it("When a function is not a property, it can be invoked directly, with the invocation operator ().", function()
        {
 	   function add(a, b) {return a+b;}
 	   expect(add(1,2)).toEqual(3);	   
        });
-    it("When function is invoded directly, the 'this' context refers to the global context.", function()
+    it("Function can be assigned to a (non-property) variable and can be invokedk with the invocation operator () applied on the variable.", function()
+       {
+	   function add (a, b) {return a+b;}
+	   var add_var  = add;
+	   expect(add_var(1,2)).toEqual(3);	   
+       });
+    it("When function is invoked directly, the 'this' context refers to the global context.", function()
        {
 	   function msg() {return this.message}
 	   expect(msg()).toEqual(message);	   
