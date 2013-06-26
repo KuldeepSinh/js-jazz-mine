d602fb3546f9ceab657563c98c271813f69f614e
Kuldeep Sinh <chauhan.kuldeep.sinh@gmail.com>
2013-06-26 11:11:31 +0200

    spec added for differential inheritance.

---
+++ 5_prototype/spec/1_inheritance.spec.js
@@ -0,0 +1,28 @@
+//A new object can inherit the properties of old object
+describe("Prototypal inheritance : ", function(){
+	it("Differential inheritance, customizes the parent object.", function(){
+
+		//following method provides cloning mechanism.
+		Object.prototype.clone = function(){
+			// Extract the prototype from the "this" object
+			var p = Object.getPrototypeOf(this);
+			// Create a new object with p as its prototype
+			return Object.create(p);
+		};
+		var mammal = {
+			kind : "Mammal",
+			get_kind : function(){
+				return this.kind;
+			}
+		};
+		var cow = mammal.clone();
+		cow.kind = "Bowine";
+		cow.legs = 4;
+		cow.graze = "grass";
+		cow.get_status = function(){
+			return "Eating " + cow.graze;
+		};
+		expect(cow.kind).toEqual("Bowine");
+		expect(cow.get_status()).toEqual("Eating grass");
+	});
+});
